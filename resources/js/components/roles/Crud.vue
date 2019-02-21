8690d6889c86153bd4f35dfdfa44341a6e081ddf
Darko Krstev <d.krstev@propellor.eu>
2019-02-21 10:12:11 +0100

    - Roles crud completed

---
+++ resources/js/components/roles/Crud.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <crud :crud-data="roles_crud"></crud>
+    </div>
+</template>
+
+<script>
+    import Crud from './../crud/Crud.vue';
+    import Form from './Form.vue';
+    import List from './List.vue';
+
+    export default {
+        components: {
+            Crud,
+        },
+        data() {
+            return {
+                roles_crud: {
+                    title: 'Roles',
+                    singular: 'role',
+                    plural: 'roles',
+                    form: Form,
+                    list: List,
+                    dt_headers: [
+                        { text: 'Name', align: 'left', sortable: true, value: 'name' },
+                        { text: 'Created at', align: 'right', value: 'created_at', sortable: true },
+                        { text: '', align: 'center', value: 'name', sortable: false }
+                    ],
+                    crud_url: '/api/roles',
+
+                    editedItem: {
+                        id: '',
+                        name: '',
+                        created_at: ''
+                    },
+                    defaultItem: {
+                        id: '',
+                        name: '',
+                        created_at: ''
+                    },
+                    extras: {
+
+                    },
+                },
+            }
+        },
+        methods: {}
+    }
+</script>
